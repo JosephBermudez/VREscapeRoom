f6592d3d744dfc9decb9ad34189fb227b81fe753
Jose Manuel Bermudez Gomez <74612587+JosephBermudez@users.noreply.github.com>
2021-11-24 16:24:07 -0600

    Level 3 minigame

---
+++ VREscapeMult/Assets/ProjectAssets/Scripts/CheckGold.cs
@@ -0,0 +1,73 @@
+// Copyright(C) Fyware SC - All Rights Reserved
+// Unauthorized copying of this project or any file, via any medium is strictly prohibited
+// Proprietary and confidential.
+// If you gain access to this project or any file and you are not the recipient, 
+// it is hereby notified that, in the event of any disclosure, reproduction, 
+// distribution or any action in relation to the content of this project, it is strictly prohibited and could be illegal. 
+// If you have received this in error, please notify us immediately by replying to contact@fyware.com and then delete it from your system.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGold : MonoBehaviour
+{
+    #region Static Variables
+
+
+
+    #endregion
+
+    #region Public Variables
+    public Level3Manager levelManager;
+
+
+    #endregion
+
+    #region Private Variables
+
+
+    #endregion
+
+    #region MonoBehaviour Callbacks
+    // Awake is called when the script instance is being loaded
+    void Awake()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelManager.totalValue = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    #endregion
+
+    #region Script Methods
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Gold")
+        {
+            levelManager.totalValue++;
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Gold")
+        {
+            levelManager.totalValue--;
+        }
+    }
+
+    #endregion
+
+}
