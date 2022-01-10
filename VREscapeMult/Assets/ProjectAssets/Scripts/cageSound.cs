93292cead2cd52c8201c2d4e4ffd8329fffd89c8
Jose Manuel Bermudez Gomez <74612587+JosephBermudez@users.noreply.github.com>
2022-01-10 16:21:38 -0600

    Colliders fixed

---
+++ VREscapeMult/Assets/ProjectAssets/Scripts/cageSound.cs
@@ -0,0 +1,59 @@
+// Copyright(C) Fyware SC - All Rights Reserved
+// Unauthorized copying of this project or any file, via any medium is strictly prohibited
+// Proprietary and confidential.
+// If you gain access to this project or any file and you are not the recipient, 
+// it is hereby notified that, in the event of any disclosure, reproduction, 
+// distribution or any action in relation to the content of this project, it is strictly prohibited and could be illegal. 
+// If you have received this in error, please notify us immediately by replying to contact@fyware.com and then delete it from your system.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cageSound : MonoBehaviour
+{
+#region Static Variables
+
+	
+
+#endregion
+
+#region Public Variables
+
+	
+
+#endregion
+
+#region Private Variables
+
+	
+
+    #endregion
+
+#region MonoBehaviour Callbacks
+	// Awake is called when the script instance is being loaded
+    void Awake()
+    {
+       
+    }
+	// Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+#endregion
+
+#region Script Methods
+
+	
+
+#endregion
+    
+}
